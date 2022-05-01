667f504abaf864fb76e5ea4750bbd116bf871d99
Conner Luzier <connerluzier@protonmail.com>
2022-04-30 23:07:40 -0400

    imgs, favicon, small tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
+import crypto from '../assets/crypto.png';
 
 function Header() {
 
     return (
         <div className="Header">
-            <h1>Merchant's Items</h1>
+            <h1><img src={crypto} alt=''></img> Crypto Merchants</h1>
         </div>
     );
 }
